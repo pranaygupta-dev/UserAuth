3f09c53781e891e9e06eb8be419a80a68bf3f112
pranaygupta-dev <pranay.gupta@tntar.io>
2025-12-10 11:04:19 +0530

    Email Service implemented

--- src/main/java/com/example/UserAuth/service/EmailService.java
+++ src/main/java/com/example/UserAuth/service/EmailService.java
@@ -1,12 +1,30 @@
 package com.example.UserAuth.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class EmailService {
 
     @Autowired
     private JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String body) {
+        // Implementation for sending email
+        try {
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(to);
+            mail.setSubject(subject);
+            mail.setText(body);
+            javaMailSender.send(mail);
+            log.info("Email sent to {}", to);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("Error sending email to {}: {}", to, e.getMessage());
+        }
+    }
 }
--- src/test/java/com/example/UserAuth/service/EmailServiceTest.java
+++ src/test/java/com/example/UserAuth/service/EmailServiceTest.java
@@ -1,7 +1,19 @@
 package com.example.UserAuth.service;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class EmailServiceTest {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void testSendEmail() {
+        emailService.sendEmail("vrajpatel9638@gmail.com",
+                "Testing Java mail sender",
+                "Hi, aap kaise hain?");
+    }
 }
